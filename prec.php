ef7aec0ad692851c2e388b65d68b7ce013919227
Jenkins <skreker99@gmail.com>
2019-12-04 21:39:46 +0200

    check

--- prec.php
+++ prec.php
@@ -1,7 +1,7 @@
 <?php
 
 $result=$_GET["data"];
-
+$count=0;
 // var_dump($result);
 $kek=['a bitter pill','blew it','black economy','bombed','went like a bomb','blue collar','bottom line','bottlenecks',' back to the drawing board','blow-by-blow'];
 
@@ -9,6 +9,9 @@ for($i=0;$i<count($result)  ;$i++){
     if(strlen($result[$i])!=0){
         echo ("вы выбрали "."<font color=red>". $result[$i] ."</font>"." правильный ответ "."<font color=blue>".$kek[$i]."</font>"."<br>");
     }
+    if($result[$i]==$kek[$i]){
+        $count+=1;
+    }
 }
-
+echo($count);
 
