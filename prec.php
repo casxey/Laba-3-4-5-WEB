2767945c3c4c31e477939730cca6235abab89fb2
Саша Кащук <58071414+casxey@users.noreply.github.com>
2019-12-04 20:17:26 +0200

    Add files via upload

---
+++ prec.php
@@ -0,0 +1,14 @@
+<?php
+
+$result=$_GET["data"];
+
+// var_dump($result);
+$kek=['a bitter pill','blew it','black economy','bombed','went like a bomb','blue collar','bottom line','bottlenecks',' back to the drawing board','blow-by-blow'];
+
+for($i=0;$i<count($result)  ;$i++){   
+    if(strlen($result[$i])!=0){
+        echo ("вы выбрали "."<font color=red>". $result[$i] ."</font>"." правильный ответ "."<font color=blue>".$kek[$i]."</font>"."<br>");
+    }
+}
+
+
